61fc1447e62e2fc55a889fec2c835a3650e8727c
Monika Tadross <monikk.tadross@gmail.com>
2019-04-11 19:50:25 +0000

    Automatically backed up by Learn

---
+++ practicejava.js
@@ -0,0 +1,21 @@
+//console.log("hello");
+//function speaker() {
+   //sentence = 'Bird is the word.';
+  
+//}
+  
+  //speaker();
+  //console.log(sentence);
+  
+  var cuteAnimal = 'quokka';
+ 
+function makeVariable() {
+  var cuteAnimal = 'sugar glider';
+  return cuteAnimal;
+}
+makeVariable();
+ 
+cuteAnimal();
+
+ 
+  
